eb2effc36a5eb216e051eaee52ec70482d76c0d4
wuhui.zhu <wuhui.zhu@platenogroup.com>
2017-02-15 18:08:34 +0800

    初步完成整个验证类

--- src/assets/js/validator.js
+++ src/assets/js/validator.js
@@ -14,7 +14,69 @@
  * 11、是否邮箱；
  * @return {[Boolean]} [验证是否通过]
  */
-var validator = function () {
-    var validationStrategies = {}; // 验证策略
-    var validate = function () {}; // 验证行为
+var Validator = function () {
+    this.strategies = {}; // 实例对象的验证策略集合
+    this.errors = []; // 验证函数队列
 }
+
+/**
+* 导入验证策略函数
+* @param  {[object]} strategies [策略函数对象（键值对形式）]
+* @return {[object]}            [null]
+*/
+Validator.prototype.importStrategies = function (strategies) {
+    if (defaultStrategies.isNoEmptyObject(strategies)) {
+        for (var name in strategies) {
+            this.addStrategies(name, strategies[name]);
+        }
+    } else {
+        console.error("导入的验证策略函数对象非Json对象");
+    }
+};
+/**
+ * 添加策略
+ * @param {[String]} name     [策略名字]
+ * @param {[Function]} strategy [策略函数体]
+ */
+Validator.prototype.addStrategies = function (name, strategy) {
+    this.strategies[name] = strategy;
+};
+/**
+ * 验证函数
+ * @param  {[String, Array...]} value      [验证的值]
+ * @param  {[String]} rule       [验证规则]
+ * @param  {[type]} ruleParams [对应验证规则的参数]
+ * @return {[type]}            [description]
+ */
+Validator.prototype.valid = function (value, ruleElement, errMsg) {
+    if (!value || !ruleElement) {
+        console.error("验证函数参数有误");
+        return;
+    }
+
+    var result,
+    ruleName,
+    ruleParams;
+    var params = [];
+    var ruleElement = ruleElement.split(":");
+
+    ruleName = ruleElement[0]; // 规则名字
+    ruleParams = ruleElement[1]; // 规则函数的参数
+    params.push(value);
+    
+    ruleParams != "" && ruleParams !== null && ruleParams !== undefined && params.push(ruleParams);
+    // 执行验证函数, 正确返回true，错误返回errMsg
+    result = this.strategies[ruleName].apply(this, params);
+    result = !!result ? result : errMsg;
+    this.errors.push(result);
+};
+
+var defaultStrategies = {
+    // 是否非空对象
+    isNoEmptyObject: function (obj) {
+        var bool = (typeof obj === "object") && (Object.prototype.toString.call(obj).toLowerCase() === "[object object]") && !obj.length;
+        return bool || false;
+    }
+}
+
+export default Validator;
--- src/views/index.vue
+++ src/views/index.vue
@@ -10,16 +10,28 @@
 </template>
 
 <script>
-import { mapActions } from "vuex"
+	// vuex
+	import { mapActions } from "vuex"
+
+	// 导入相关工具
+	import Validator from "assets/js/validator"
 	export default {
 		data() {
 			return {}
 		},
 		computed: {},
 		mounted() {
-			// console.log(this.$store);
 			this.setGoodsName("篮球");
-			// this.$store.dispatch("setGoodsName", "篮球");
+
+			let formValidator = new Validator();
+			formValidator.importStrategies({
+				isNumber: function (value) {
+					return !isNaN(value);
+				}
+			});
+			formValidator.valid("faf", "isNumber", "不是数字");
+			formValidator.valid("faf", "isNumber", "肯定不是数字");
+			console.warn(formValidator.errors);
 		},
 		methods: {
 			...mapActions([
